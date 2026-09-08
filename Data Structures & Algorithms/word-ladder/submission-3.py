d4b5f85b51b7198277fa3b9f1ac5165d3492431c
sejung <sejungkim129@gmail.com>
2026-09-07 21:42:46 -0400

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        variations = {}
+
+        for word in wordList:
+            for i in range(len(word)):
+                key = word[:i] + "*" + word[i + 1:]
+                variations.setdefault(key, []).append(word)
+
+        queue = deque([(beginWord, 1)])
+        visited = set()
+        visited.add(beginWord)
+        while queue:
+            word, dist = queue.popleft()
+            if word == endWord:
+                return dist
+
+            
+            for i in range(len(word)):
+                key = word[:i] + "*" + word[i+1:]
+                if not key in variations:
+                    continue
+                
+                for neighbor in variations[key]:
+                    if neighbor not in visited:
+                        queue.append((neighbor, dist + 1))
+                        visited.add(neighbor)
+
+        return 0
